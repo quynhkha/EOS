ca415c35eb39a2f5e17a0b20afa7ad2ee59eea75
Long Nguyen <ndlong95@gmail.com>
2017-09-14 01:08:50 +0800

    brush tool works! Need to integrate to main code

--- ImageProcessing/cv2_mousetracking.py
+++ ImageProcessing/cv2_mousetracking.py
@@ -1,51 +1,99 @@
 import cv2
 import numpy as np
 
+import copy
+
 is_drawing = False
 is_add = True
+show_brush_region = True
 brush_size = 5
 ix, iy = -1, -1
 img = cv2.imread('/home/long/PycharmProjects/EOS/ImageProcessing/data/1947-1_plg6.small.png')
+############### Note ##############
+# need to convert img to uint8 to have same type with final mask apply on image (so that cv2.addWeighted can run)
+# need to use np.int8 mask instead of np.uint8 mask for calculation to avoid overflow when value becomes negative
 img = np.uint8(img)
+mask_add = np.zeros(img[:, :, 0].shape, np.int8) #int8: -128 ->127
+mask_subtract = np.zeros(img[:, :, 0].shape, np.int8)
+mask_int8 = np.zeros(img[:, :, 0].shape, np.int8)
 mask = np.zeros(img[:, :, 0].shape, np.uint8)
-mask_red = np.zeros(img.shape, np.uint8)
+
+mask_color = np.zeros(img.shape, np.uint8)
 result_img = np.zeros(img.shape, np.uint8)
+
+img_copy = copy.copy(img)
+simulated_mask = np.zeros(img[:, :, 0].shape, np.uint8)
+cv2.rectangle(simulated_mask,(200,0),(400,128),255,-1)
+print("here once")
+
+# io.imshow(simulated_mask)
+# io.show()
 
 # mouse callback
 def mouse_tracking(event, x, y, flags, param):
-    global ix, iy, is_drawing, is_add, mask, mask_red, result_img
+    global ix, iy, is_drawing, is_add, mask, mask_color, mask_int8, result_img, img_copy
 
     if event == cv2.EVENT_LBUTTONDOWN:
         is_drawing = True
         ix, iy = x, y
+        mask_add[mask_int8 == 0] = 0
+        mask_subtract[mask_int8 == 0] = 0
 
     elif event == cv2.EVENT_MOUSEMOVE:
         if is_drawing == True:
             if is_add == True:
-                cv2.circle (mask, (x,y), brush_size, 255, -1)
+                # mask_add[mask_int8 == 0] = 0
+                # mask_subtract[mask_int8 == 0] = 0
+                cv2.circle (mask_add, (x,y), brush_size, 64, -1)
+                mask_int8 = mask_add - mask_subtract  # value -64, 0, 64: subtract, no op, add
+
             elif is_add == False:
-                cv2.circle(mask, (x, y), brush_size, 0, -1)
-            mask_red[mask == 255] = (0, 0, 255)
-            mask_red[mask == 0] = (0, 0, 0)
+                # mask_add[mask_int8 == 0] = 0
+                # mask_subtract[mask_int8 == 0] = 0
+                cv2.circle(mask_subtract, (x, y), brush_size, 64, -1)
+                mask_int8 = mask_add - mask_subtract  # value -64, 0, 64: subtract, no op, add
 
     elif event == cv2.EVENT_LBUTTONUP:
         is_drawing = False
         if is_add == True:
-            cv2.circle(mask, (x, y), brush_size, 255, -1)
-        elif is_add == False:
-            cv2.circle(mask, (x, y), brush_size, 0, -1)
-        mask_red[mask == 255] = (0, 0, 255)
-        mask_red[mask == 0] = (0, 0, 0)
+            # mask_add[mask_int8 == 0] = 0
+            # mask_subtract[mask_int8 == 0] = 0
+            cv2.circle(mask_add, (x, y), brush_size, 64, -1)
+            mask_int8 = mask_add - mask_subtract  # value -64, 0, 64: subtract, no op, add
 
-    result_img = cv2.addWeighted(img,0.7,mask_red,0.3,0)
+        elif is_add == False:
+            # mask_add[mask_int8 == 0] = 0
+            # mask_subtract[mask_int8 == 0] = 0
+            cv2.circle(mask_subtract, (x, y), brush_size, 64, -1)
+            mask_int8 = mask_add - mask_subtract  # value -64, 0, 64: subtract, no op, add
+
+
+    #mask_int8 = np.int8(np.int16(mask_add) - np.int16(mask_subtract))
+    #mask_int8 = mask_add - mask_subtract  # value -64, 0, 64: subtract, no op, add
+    mask_color[mask_int8 == 64] = (0, 255, 0)
+    mask_color[mask_int8 == 0] = (0, 0, 0)
+    mask_color[mask_int8 == -64] = (0, 0, 255)
+
+    simulated_mask[mask_int8==64] = 255
+    simulated_mask[mask_int8==-64] = 0
+
+    img_copy = copy.copy(img)
+    img_copy[simulated_mask==0] = 0
+
+    #result_img = cv2.addWeighted(img,0.7,mask_color,0.3,0)
+    result_img = cv2.addWeighted(img_copy, 0.7, mask_color, 0.3, 0)
 cv2.namedWindow('image')
 cv2.setMouseCallback('image', mouse_tracking)
 
+
 while(1):
-    cv2.imshow('image', result_img)
+    if show_brush_region:
+        cv2.imshow('image', result_img)
+    else:
+        cv2.imshow('image', img_copy)
     k = cv2.waitKey(1) & 0xFF
-    # if k == ord('m'):
-    #     mode = not mode
+    if k == ord('m'):
+        show_brush_region = not show_brush_region
 
     if k == ord('['):
         if brush_size >1:
@@ -59,7 +107,7 @@ while(1):
     elif k == ord ('t'):
         is_add = not is_add
         print('is_add', is_add)
-    elif k == 27:
+    elif k == 27: # ESC key pressed
         break
 
 cv2.destroyAllWindows()
