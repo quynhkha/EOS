07643458a50ede5a308ce83e0e268ef5e36b8c57
Long Nguyen <ndlong95@gmail.com>
2017-11-15 20:46:30 +0800

    refactor

--- WebMockUp/WebMockUp/settings.py
+++ WebMockUp/WebMockUp/settings.py
@@ -120,7 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
+
+STATIC_URL = '/static/' # the path in url
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
