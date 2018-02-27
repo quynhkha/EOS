b6bf5756e25b460d7b76ec3f0e4cbb538abaaa77
Long Nguyen <ndlong95@gmail.com>
2018-02-27 15:05:22 +0800

    fix

--- WebMockUp/WebMockUp/imageProcessing/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImageprocessingConfig(AppConfig):
-    name = 'WebMockUp.imageProcessing'
--- WebMockUp/static/js/init_processing_page.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-    $.getScript("./processing_page.js");
-});
