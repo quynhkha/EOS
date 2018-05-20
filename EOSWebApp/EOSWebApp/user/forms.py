b7186930c9f8a1467675f98848cdc6d267a0ddd6
Khoi Pham <khoipham2016@gmail.com>
2018-05-21 00:35:10 +0800

    Hide password field visibility of registration

--- EOSWebApp/EOSWebApp/user/forms.py
+++ EOSWebApp/EOSWebApp/user/forms.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import User
-from django.forms import forms, ModelForm
+from django import forms
 
 
-class UserForm(ModelForm):
-    # password = forms.CharField(widget=forms.PasswordInput) # for showing the password as hidden text
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput) # for showing the password as hidden text
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'password', 'email']
